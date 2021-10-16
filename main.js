4d64d491461be7779a0707216bc58caa4e72b805
unknown <kalbirdhammi@gmail.com>
2021-10-16 20:03:11 +0100

    Added arrays for random message

---
+++ main.js
@@ -0,0 +1,5 @@
+const charaRace = ['Dragonborn', 'Dwarf', 'Elf', 'Gnome', 'Half-Elf', 'Halfling', 'Half-Orc', 'Human', 'Tiefling'];
+const charaClass = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', 'Paladin', 'Ranger', 'Rogue', 'Sorcerer', 'Warlock', 'Wizard'];
+const charaBg = ['Acolyte', 'Charlatan', 'Criminal', 'Criminal (Spy)', 'Entertainer', 'Entertainer (Gladiator)', 'Folk Hero', 'Guild Artisan', 'Guild Artisan (Guild Merchant)', 'Hermit', 'Noble', 'Noble (Knight)', 'Outlander', 'Sage', 'Sailor', 'Sailor (Pirate)', 'Soldier', 'Urchin'];
+const charaGender = ['Male', 'Female', 'Non-Binary'];
+
