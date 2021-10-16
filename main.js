bdda14036391d0bc16cdcfe65242bf906d118e2b
unknown <kalbirdhammi@gmail.com>
2021-10-16 20:17:30 +0100

    Completed basic program that satisfies the overview

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const charaClass = ['Barbarian', 'Bard', 'Cleric', 'Druid', 'Fighter', 'Monk', '
 const charaBg = ['Acolyte', 'Charlatan', 'Criminal', 'Criminal (Spy)', 'Entertainer', 'Entertainer (Gladiator)', 'Folk Hero', 'Guild Artisan', 'Guild Artisan (Guild Merchant)', 'Hermit', 'Noble', 'Noble (Knight)', 'Outlander', 'Sage', 'Sailor', 'Sailor (Pirate)', 'Soldier', 'Urchin'];
 const charaGender = ['Male', 'Female', 'Non-Binary'];
 
-const generateRandomMessage = () => { // generate a random message
+const generateRandomCharaAssignment = () => { // generate a random message
     let randRace = charaRace[Math.floor(Math.random()*charaRace.length)]; // picks a random index from the charaRace array and assigns it to randRace
     let randClass = charaClass[Math.floor(Math.random()*charaClass.length)]; // picks a random index from the charaClass array and assigns it to randClass
     let randBg = charaBg[Math.floor(Math.random()*charaBg.length)]; // picks a random index from the charaBg array and assigns it to randBg
@@ -12,5 +12,6 @@ const generateRandomMessage = () => { // generate a random message
     //console.log(randClass);
     //console.log(randBg);
     //console.log(randGender);
+    console.log(`Your random character will be a ${randGender} ${randRace} ${randClass}, with the ${randBg} background.`);
 }
-generateRandomMessage();
+generateRandomCharaAssignment();
